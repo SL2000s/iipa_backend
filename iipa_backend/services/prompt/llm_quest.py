1dfe983585aad7ced8deb45bd91e228cb01307e0
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2024-10-14 10:34:57 +0200

    Add llm_quest help method

--- iipa_backend/services/prompt/llm_quest.py
+++ iipa_backend/services/prompt/llm_quest.py
@@ -1,2 +1,43 @@
+import os
+from typing import Dict
+
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import AzureChatOpenAI
+
+from config import (
+    PROMPT_TEMPLATE_FORMAT,
+    OPENAI_MODEL_NAME,
+    OPENAI_TEMPERATURE,
+    OPENAI_API_TYPE,
+    OPENAI_API_VERSION,
+    OPENAI_DEPLOYMENT_NAME,
+    OPENAI_TOP_P,
+    OPENAI_SEED,
+)
+
+
+def openai_quest(prompt_template: PromptTemplate, template_variables: Dict = {}):
+    client = AzureChatOpenAI(
+        model_name=OPENAI_MODEL_NAME,
+        temperature=OPENAI_TEMPERATURE,
+        openai_api_type=OPENAI_API_TYPE,
+        openai_api_version=OPENAI_API_VERSION,
+        deployment_name=OPENAI_DEPLOYMENT_NAME,
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        azure_endpoint=os.getenv("AZURE_ENDPOINT"),
+        top_p=OPENAI_TOP_P,
+        seed=OPENAI_SEED,
+    )
+    chain = prompt_template | client
+    chain_results = chain.invoke(template_variables)
+    result_text = chain_results.content
+    return result_text#.strip('\n').strip()
+
+
 def llm_quest(prompt):
-    return "PROMPT ANSWER [TODO]"
+    prompt_template = PromptTemplate.from_template(
+        prompt,
+        template_format=PROMPT_TEMPLATE_FORMAT,
+    )
+    llm_ans = openai_quest(prompt_template)
+    return llm_ans
