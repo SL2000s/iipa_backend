9ee5dbcdf3bd4e5be4513b9fcee2865d4a0a170d
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2024-10-16 14:35:08 +0200

    Remove unused import

--- iipa_backend/main.py
+++ iipa_backend/main.py
@@ -1,5 +1,4 @@
 # from app.routes import tactics, kb                                            # TODO: implement or remove
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
